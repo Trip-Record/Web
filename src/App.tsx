4b599e2b0e9694a7cc527356ce3cf91057e56806
조민기 <58856846+Nail0606@users.noreply.github.com>
2023-12-30 02:57:48 +0900

    Feat : 라우팅 추가 및 기본 링크 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,18 @@
 import "./App.css";
+import { Routes, Route, Link } from "react-router-dom";
+import RegisterPage from "./components/RegisterPage";
+import LoginPage from "./components/LoginPage";
 
 function App() {
   return (
-    <div className="flex flex-col w-2/3 m-auto mt-40 items-center bg-slate-300 text-center">
-      <h1 className="text-3xl mb-4">TravelMaker 로고</h1>
-      <h1 className="text-3xl mb-4">로그인</h1>
-      <form className="flex flex-col w-2/3 mb-4">
-        <input type="text" name="id" placeholder="아이디를 입력해주세요" />
-        <input
-          type="password"
-          name="pw"
-          placeholder="비밀번호를 입력해주세요"
-        />
-        <input type="submit" value="로그인" className=" bg-slate-500" />
-      </form>
-      <div className="text-center">
-        <div className="flex justify-start">
-          <div className="mr-20 ">구글 간편 로그인</div>
-          <div className=" "> 이미지</div>
-        </div>
-        <div className="flex justify-center">
-          <div className="mr-10 ">이메일로 회원가입하기</div>
-          <button className=" ">회원가입 버튼</button>
-        </div>
-      </div>
+    <div>
+      <Link to="/login">로그인 페이지 이동</Link>
+      <Link to="/register">회원가입 페이지 이동</Link>
+      <Routes>
+        <Route path="/" element={<div>메인 페이지</div>} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
     </div>
   );
 }
