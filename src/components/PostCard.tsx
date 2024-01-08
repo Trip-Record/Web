f2e1a1c2fc93143f8a713debdb2a62ea1b181256
Goongam <dlwjddn341@naver.com>
2024-01-08 22:43:09 +0900

    UI : 포스트카드 갭

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -12,7 +12,7 @@ export default function PostCard({ post }: Props) {
   return (
     <section className="flex flex-row justify-center w-full h-56 bg-white border-b last:border-b-white p-2">
       {/* 아바타 / 사용자이름 */} {/* 여행타입 */}
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-1">
         <div className="flex items-center gap-1">
           <Avatar img="/logo192.png" size="s" />
           <span className="text-gray-400">이름</span>
@@ -22,8 +22,8 @@ export default function PostCard({ post }: Props) {
         <h2 className="font-bold line-clamp-1">{title}</h2>
         <div className="line-clamp-4">{body}</div>
         <div className="mt-auto">
-          <span>좋아요</span>
-          <span>댓글</span>
+          <span>좋아요 1 </span>
+          <span>댓글 1</span>
         </div>
       </div>
       <img
