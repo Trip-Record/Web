c6756bb5ed3ed76c34fe34af489cf291fe8b593e
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-01-08 23:59:00 +0900

    UI : 기록 입력 페이지 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { Routes, Route, Link } from "react-router-dom";
 import RegisterPage from "./components/RegisterPage";
 import LoginPage from "./components/LoginPage";
 import SelectTravelStyle from "./components/SelectTravelStyle";
+import NewPostInputPage from "./components/NewPostInputPage";
 
 function App() {
   return (
     <div className=" h-full">
       <Link to="/login">로그인 페이지 이동</Link>
       <Link to="/register">회원가입 페이지 이동</Link>
+      <Link to="/newPostInputPage">기록 입력 페이지 이동</Link>
       <Routes>
         <Route path="/" element={<div>메인 페이지</div>} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/test" element={<SelectTravelStyle />} />
+        <Route path="/newPostInputPage" element={<NewPostInputPage />} />
       </Routes>
     </div>
   );
