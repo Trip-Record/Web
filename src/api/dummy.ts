cdcd3c14a646e21e6a6c904002f64dd700298507
Goongam <dlwjddn341@naver.com>
2024-01-08 22:22:30 +0900

    Feat : 임시 포스트 fetch query

--- src/api/dummy.ts
+++ src/api/dummy.ts
@@ -7,6 +7,13 @@ export interface Data {
   completed: boolean;
 }
 
+export interface PostData {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
 export const api = createApi({
   reducerPath: "todos",
   // highlight-end
@@ -18,7 +25,11 @@ export const api = createApi({
     getTodos: builder.query<Data[], void>({
       query: () => `todos`,
     }),
+    // TODO: 실제 데이터가 오면 배열로 바꿀것 : PostData[]
+    getPosts: builder.query<PostData, number>({
+      query: (page) => `posts/${page}`,
+    }),
   }),
 });
 
-export const { useGetTodosQuery } = api;
+export const { useGetTodosQuery, useGetPostsQuery } = api;
