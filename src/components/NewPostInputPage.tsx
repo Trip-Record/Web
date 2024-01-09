29d8b6c742f87568bcf118b4c6c342d68a1566cf
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-01-09 23:57:39 +0900

    UI : 기록 입력 페이지 기본 뼈대 생성

--- src/components/NewPostInputPage.tsx
+++ src/components/NewPostInputPage.tsx
@@ -1,3 +1,41 @@
+import RegisterStringInput from "./form/RegisterInput";
+import { useState } from "react";
+import { FormError } from "./RegisterPage";
+
 export default function NewPostInputPage() {
-  return <div>기록 입력 페이지입니다.</div>;
+  const [travelName, setTravelName] = useState("");
+  const [error, setError] = useState<FormError>({});
+  return (
+    <div className="flex flex-col w-1/2 bg-slate-400 mx-auto">
+      <div>대충 로고</div>
+      <RegisterStringInput
+        label="여행 제목"
+        placeholder="기록할 여행의 제목을 입력해주세요"
+        setValue={setTravelName}
+        value={travelName}
+        type="name"
+        error={error}
+        inputType="text"
+      />
+      <RegisterStringInput
+        label="여행 제목"
+        placeholder="여행 장소를 선택해주세요"
+        setValue={setTravelName}
+        value={travelName}
+        type="name"
+        error={error}
+        inputType="text"
+      />
+      <RegisterStringInput
+        label="여행 제목"
+        placeholder="여행 기간을 선택해주세요"
+        setValue={setTravelName}
+        value={travelName}
+        type="name"
+        error={error}
+        inputType="text"
+      />
+      <textarea placeholder="내용" rows={10}></textarea>
+    </div>
+  );
 }
