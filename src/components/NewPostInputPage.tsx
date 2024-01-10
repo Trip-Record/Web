9f8599cf6dae3a87a445a0a0b027b7b6964fef82
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-01-10 18:51:07 +0900

    Feat : 데이터 추가

--- src/components/NewPostInputPage.tsx
+++ src/components/NewPostInputPage.tsx
@@ -5,6 +5,31 @@ import { FormError } from "./RegisterPage";
 export default function NewPostInputPage() {
   const [travelName, setTravelName] = useState("");
   const [error, setError] = useState<FormError>({});
+
+  const TRAVEL_AREA = {
+    국내: [
+      "서울",
+      "김포",
+      "제주",
+      "부산",
+      "광주",
+      "여수",
+      "대구",
+      "청주",
+      "여수",
+    ],
+    일본: [
+      "도쿄",
+      "오사카",
+      "후쿠오카",
+      "삿포로",
+      "오키나와",
+      "나고야",
+      "구마모토",
+      "키타큐슈",
+    ],
+  };
+
   return (
     <div className="flex flex-col w-1/2 bg-slate-400 mx-auto">
       <div>대충 로고</div>
