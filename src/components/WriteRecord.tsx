7b125c909b0e297547e3193eb1bf0a612c736605
Sunho-97 <cow528@naver.com>
2024-01-10 16:39:43 +0900

    UI : 여행 기록 작성 페이지 생성

---
+++ src/components/WriteRecord.tsx
@@ -0,0 +1,9 @@
+export default function WriteRecord() {
+  return (
+    <div className="WriteRecord">
+      <div className="bg-blue-400 h-screen">
+        <h2>기록 작성하기 페이지 입니다.</h2>
+      </div>
+    </div>
+  );
+}
