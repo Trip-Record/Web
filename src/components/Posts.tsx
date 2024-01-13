e14b16ac4f0afc97d26c80d949caabe319202c6c
Goongam <dlwjddn341@naver.com>
2024-01-13 23:24:25 +0900

    Feat : 페이지 이동시 refetch

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,24 +1,44 @@
 import { useSearchParams } from "react-router-dom";
-import { useGetPostsQuery } from "../api/dummy";
+import { useAddCommentsMutation, useGetPostsQuery } from "../api/dummy";
 import PostCard from "./PostCard";
 import PageNation from "./ui/PageNation";
+import { useEffect } from "react";
 
 export default function Posts() {
   const [searchParams] = useSearchParams();
   const page = Number(searchParams.get("page") ?? 1);
-  const { data, isLoading } = useGetPostsQuery(page);
+  const { data, isLoading, isFetching, refetch } = useGetPostsQuery(page);
+  const [updatePost, result] = useAddCommentsMutation();
 
+  console.log(result.data);
+
+  // TODO: 백엔드 Api 완성후 캐시 데이터 사용해보기
+  useEffect(() => {
+    refetch();
+  }, [page, refetch]);
   // 임시로 배열로 만들어서 작업 중... 백엔드 API완성 후 삭제예정
-  const posts = [data, data, data, data, data];
+  const posts = [data, data, data, data];
 
-  if (isLoading) return <div>loading...</div>;
+  if (isLoading || isFetching) return <>loading...</>;
 
   return (
     <main className="flex flex-col items-center justify-center w-full bg-white px-10">
+      <button
+        onClick={() => {
+          updatePost({
+            id: 1,
+            body: "tesbody",
+            email: "testemail",
+            name: "testname",
+          });
+        }}
+      >
+        test
+      </button>
       {posts.map(
         (post, i) => post && <PostCard post={post} key={post.id + i} />
       )}
-      <PageNation maxPage={12} showPage={10} />
+      <PageNation maxPage={12} showPage={5} />
     </main>
   );
 }
