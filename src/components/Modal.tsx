c4a0c9f4274773941bbb4a0f405e2648212277a7
Goongam <dlwjddn341@naver.com>
2024-01-14 22:14:26 +0900

    Feat : 모달 클래스 수정, 커스텀 클래스이름 props 추가

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -3,15 +3,16 @@ import { useRef } from "react";
 interface Props {
   button: React.ReactNode;
   modal: React.ReactNode;
+  buttonClassName?: string;
 }
-export default function ModalButton({ button, modal }: Props) {
+export default function ModalButton({ button, modal, buttonClassName }: Props) {
   const ref = useRef<HTMLDialogElement>(null);
 
   return (
     <>
       <dialog
         ref={ref}
-        onClick={(e) => {
+        onMouseDown={(e) => {
           if (e.target === e.currentTarget) ref.current?.close();
         }}
         className="backdrop:bg-black/50 max-w-[100vw]"
@@ -19,6 +20,7 @@ export default function ModalButton({ button, modal }: Props) {
         {modal}
       </dialog>
       <div
+        className={`w-full ${buttonClassName}`}
         onClick={() => {
           ref.current?.showModal();
         }}
