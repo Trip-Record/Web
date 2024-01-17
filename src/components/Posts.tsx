298742dbfb96ca2d9fbe0790a243201d7f2275b5
Goongam <59019137+Goongam@users.noreply.github.com>
2024-01-17 20:16:46 +0900

    Merge : Postcard instagram (#12)
    * Feat : 인스타그램 형 포스트 컴포넌트
    
    * Feat : 포스트 타입 변경 버튼

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -2,12 +2,16 @@ import { useSearchParams } from "react-router-dom";
 import { useAddCommentsMutation, useGetPostsQuery } from "../api/dummy";
 import PostCard from "./PostCard";
 import PageNation from "./ui/PageNation";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import BlogIcon from "./ui/icons/BlogIcon";
+import CardIcon from "./ui/icons/CardIcon";
 
 export default function Posts() {
   const [searchParams] = useSearchParams();
   const page = Number(searchParams.get("page") ?? 1);
   const { data, isLoading, isFetching, refetch } = useGetPostsQuery(page);
+
+  const [cardType, setCardType] = useState<"blog" | "instagram">("blog");
 
   // TODO: 백엔드 Api 완성후 캐시 데이터 사용해보기
   useEffect(() => {
@@ -19,9 +23,18 @@ export default function Posts() {
   if (isLoading || isFetching) return <>loading...</>;
 
   return (
-    <main className="flex flex-col items-center justify-center w-full bg-white px-10">
+    <main className="flex flex-col items-center justify-center w-full bg-white px-10 py-5 gap-5">
+      <div className="ml-auto border flex items-center p-1">
+        <button className="border-r p-2" onClick={() => setCardType("blog")}>
+          <BlogIcon />
+        </button>
+        <button className="p-2" onClick={() => setCardType("instagram")}>
+          <CardIcon />
+        </button>
+      </div>
       {posts.map(
-        (post, i) => post && <PostCard post={post} key={post.id + i} />
+        (post, i) =>
+          post && <PostCard post={post} key={post.id + i} type={cardType} />
       )}
       <PageNation maxPage={12} showPage={5} />
     </main>
