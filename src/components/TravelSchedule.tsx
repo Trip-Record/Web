11ebf3ef963aa62db2e0cd5cbd7485fd1c14f9d1
조민기 <58856846+Nail0606@users.noreply.github.com>
2024-01-25 23:57:53 +0900

    UI : 일정

--- src/components/TravelSchedule.tsx
+++ src/components/TravelSchedule.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom";
 
 export default function TravelSchedule() {
   return (
-    <div className="bg-red-200 h-screen">
-      <h2>여행 일정 페이지 입니다.</h2>
+    <div className="bg-red-200 h-screen flex flex-col">
+      <div className="flex items-center justify-between border-b border-black/80 pb-2">
+        <div className="flex gap-2 text-gray-500 flex-col md:flex-row">
+          <span>대한민국, 부산</span>
+          <span>2023.10.22-2023.10.24</span>
+        </div>
 
-      <div className="write-container">
         <Link to="/write-schedule" className="">
           일정 작성하기
         </Link>
