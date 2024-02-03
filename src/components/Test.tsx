3eda96eead1b033405c2b8457f4c8c18b0d8fe52
Goongam <dlwjddn341@naver.com>
2024-02-03 23:19:15 +0900

    Feat : endpoint 수정

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,6 +1,6 @@
 export default function Test() {
   const register = () => {
-    fetch("http://15.164.19.143:8080/user/signup", {
+    fetch("http://15.164.19.143:8080/users/signup", {
       method: "post",
       body: JSON.stringify({
         userEmail: "테스트계정w@gmail.com",
@@ -13,14 +13,27 @@ export default function Test() {
   };
 
   const login = () => {
-    fetch("http://15.164.19.143:8080/");
+    fetch("http://15.164.19.143:8080/users/login", {
+      method: "post",
+      body: JSON.stringify({
+        userEmail: "testexception@naver.com",
+        userPassword: 1111,
+      }),
+    });
+  };
+
+  const test = () => {
+    fetch("http://15.164.19.143:8080/trip-styles")
+      .then((res) => res.json())
+      .then((data) => console.log(data));
   };
   return (
     <div>
       {" "}
       텥스트 페이지
       <button onClick={register}>회워나입</button>
-      <button onClick={login}>러인</button>
+      <button onClick={login}>로그인</button>
+      <button onClick={test}>테스트</button>
     </div>
   );
 }
