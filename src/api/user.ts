1d568982efb315d3e818a2e691dda618adfd1ec1
Goongam <dlwjddn341@naver.com>
2024-02-10 00:36:40 +0900

    Refactor : 유저정보 가져오기 rtkquery로 마이그레이션

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,16 +1,29 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { getLoginToken } from "../services/storage";
+import { UserInfo } from "../hooks/useUser";
 
-export const api = createApi({
+export const userApi = createApi({
   reducerPath: "user",
   // highlight-end
   baseQuery: fetchBaseQuery({
     baseUrl: "http://15.164.19.143:8080",
+    // headers: { Authorization: `Bearer ${getLoginToken()}` },
+    prepareHeaders: (headers, { getState }) => {
+      const token = getLoginToken();
+
+      // If we have a token set in state, let's assume that we should be passing it.
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
-    getUserInfo: builder.query<any, void>({
+    getUserInfo: builder.query<UserInfo, void>({
       query: () => `users/informations`,
     }),
   }),
 });
 
-export const { useGetUserInfoQuery } = api;
+export const { useGetUserInfoQuery } = userApi;
--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { getLoginToken, setLoginToken } from "../services/storage";
 import { HOST } from "../constants";
+import { useGetUserInfoQuery } from "../api/user";
 
 export interface UserProfile {
   userNickname: string;
@@ -17,7 +18,7 @@ export interface UserInfo {
 }
 
 export interface UserInfoWithToken extends UserInfo {
-  token: string;
+  token: string | null;
 }
 
 // 1. 토큰이 있는지 확인
@@ -25,36 +26,18 @@ export interface UserInfoWithToken extends UserInfo {
 //  1-2. 토큰이 있다면 서버에서 유저데이터 받아오기
 // 2. 유저데이터 리턴
 // 3. 만약 만료된 토큰이며 에러라면 토큰삭제(로그아웃과 같은 프로세스)
-export function useUser() {
-  const [user, setUser] = useState<UserInfoWithToken | null>(null);
+export function useUser(): {
+  user: UserInfoWithToken | null;
+  logout: () => void;
+} {
+  // const [user, setUser] = useState<UserInfoWithToken | null>(null);
   const token = getLoginToken();
-
-  useEffect(() => {
-    if (!token) setUser(null);
-    else {
-      //TODO: 서버와 통신하고 유저 데이터를 받아오는 코드
-      // 로딩 스테이트 필요시 추가
-      fetch(HOST + "/users/informations", {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${token}`,
-        },
-      })
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data);
-          setUser(data);
-        });
-    }
-  }, [token]);
-
-  // useEffect(() => {}, []);
-
+  const { data } = useGetUserInfoQuery(undefined, { skip: !token });
   const logout = () => {
     setLoginToken("");
     window.location.reload();
   };
 
-  return { user, logout };
+  const user = data ? { ...data, token } : null;
+  return { logout, user };
 }
