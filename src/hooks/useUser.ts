a569f820f9308e5bfc3029c22eb7599438399464
Goongam <dlwjddn341@naver.com>
2024-02-09 23:49:50 +0900

    Feat : useUser훅에서 유저정보 가져오기

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -1,8 +1,22 @@
 import { useEffect, useState } from "react";
 import { getLoginToken, setLoginToken } from "../services/storage";
+import { HOST } from "../constants";
 
+export interface UserProfile {
+  userNickname: string;
+  userProfileImg: string;
+  userTripStyleImg: string;
+  userTripStyleName: string;
+}
 export interface UserInfo {
-  email: string;
+  likeTotal: number;
+  placeTotal: number;
+  recordTotal: number;
+  scheduleTotal: number;
+  userProfile: UserProfile;
+}
+
+export interface UserInfoWithToken extends UserInfo {
   token: string;
 }
 
@@ -12,7 +26,7 @@ export interface UserInfo {
 // 2. 유저데이터 리턴
 // 3. 만약 만료된 토큰이며 에러라면 토큰삭제(로그아웃과 같은 프로세스)
 export function useUser() {
-  const [user, setUser] = useState<UserInfo | null>(null);
+  const [user, setUser] = useState<UserInfoWithToken | null>(null);
   const token = getLoginToken();
 
   useEffect(() => {
@@ -20,7 +34,18 @@ export function useUser() {
     else {
       //TODO: 서버와 통신하고 유저 데이터를 받아오는 코드
       // 로딩 스테이트 필요시 추가
-      setUser({ email: "asd@asd.com", token: token });
+      fetch(HOST + "/users/informations", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log(data);
+          setUser(data);
+        });
     }
   }, [token]);
 
