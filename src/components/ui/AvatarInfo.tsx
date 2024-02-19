4e012e1544efa5da0828bb962991f0dc7d0942d8
Goongam <dlwjddn341@naver.com>
2024-02-19 21:50:03 +0900

    Feat : 유저 아바타 가져오기

--- src/components/ui/AvatarInfo.tsx
+++ src/components/ui/AvatarInfo.tsx
@@ -1,20 +1,24 @@
+import { UserProfile } from "../../hooks/useUser";
 import Avatar from "./Avatar";
 import TravelStyle from "./TravelStyle";
 
 interface Props {
-  userId: number;
+  userProfile?: UserProfile;
+  userId?: number;
 }
 
-export default function AvatarInfo({ userId }: Props) {
-  //TODO: userId로 사용자 정보 가져오는 작업 필요
+export default function AvatarInfo({ userProfile }: Props) {
+  if (!userProfile) return <></>;
+
+  const { userNickname, userProfileImg, userTripStyleName } = userProfile;
 
   return (
     <div className="flex w-full flex-row gap-1 items-center">
-      <div className="flex items-center">
-        <Avatar img="/logo192.png" size="s" />
-        <span>이름</span>
+      <div className="flex items-center gap-1">
+        <Avatar img={userProfileImg} size="s" />
+        <span>{userNickname}</span>
       </div>
-      <TravelStyle selectStyle="인생 사진형" />
+      <TravelStyle selectStyle={userTripStyleName} />
     </div>
   );
 }
