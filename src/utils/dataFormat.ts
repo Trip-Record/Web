8b6026b66636b3ef2c34174bca869edf0d328ea8
Goongam <dlwjddn341@naver.com>
2024-03-07 21:10:35 +0900

    fix : 댓글 등록 시 날짜 표시 수정

--- src/utils/dataFormat.ts
+++ src/utils/dataFormat.ts
@@ -5,3 +5,22 @@ export function formatPlace(recordPlaces: Place[]) {
     .map((place) => `${place.countryName}, ${place.placeName}`)
     .join(" / ");
 }
+
+export function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const dateString = year + "." + month + "." + day;
+  return dateString;
+}
+
+export function formatTime(date: Date) {
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+  const time = date.getHours().toString().padStart(2, "0");
+  const m = date.getMinutes().toString().padStart(2, "0");
+
+  const dateString = year + "." + month + "." + day + " " + time + ":" + m;
+  return dateString;
+}
