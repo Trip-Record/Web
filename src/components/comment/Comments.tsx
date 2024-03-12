e65938cfd248de48dc77727b7ef431df548654ce
Goongam <dlwjddn341@naver.com>
2024-03-12 23:19:13 +0900

    fix : 코멘트 맨뒤로 이동

--- src/components/comment/Comments.tsx
+++ src/components/comment/Comments.tsx
@@ -1,6 +1,6 @@
 import CommentLine from "../comment/CommentLine";
 import ColorButton from "../ui/ColorButton";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useInput } from "../../hooks/useInput";
 import Avatar from "../ui/Avatar";
 import { useUser } from "../../hooks/useUser";
@@ -19,10 +19,11 @@ interface Props {
   commentCount: number;
 }
 export default function Comments({ postId, commentCount }: Props) {
+  // TODO: 마지막페이지로 이동
   const { user } = useUser();
   const page = useCurrentPage();
   const navigate = useNavigate();
-  const { data } = useGetComments2Query({
+  const { data, refetch } = useGetComments2Query({
     recordId: postId,
     page,
   });
@@ -37,9 +38,17 @@ export default function Comments({ postId, commentCount }: Props) {
 
   useEffect(() => {
     if (isSuccess) {
-      navigate("?page=1");
+      refetch().then((data) => {
+        const page = data.data?.totalPages;
+        navigate("?page=" + page);
+      });
     }
-  }, [isSuccess, navigate]);
+  }, [isSuccess, navigate, refetch]);
+
+  // 페이지 이동후 refetch
+  useEffect(() => {
+    refetch();
+  }, [page, refetch]);
 
   if (!data) return <>Loading...</>;
   const comments = data.recordComments;
